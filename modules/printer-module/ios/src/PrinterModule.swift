7535aa1991a43011e299ecbb4580dbe33d178e82
Deryan Argueta <deryan@ravn.co>
2025-05-19 17:50:11 -0600

    remove image from sample data

--- modules/printer-module/ios/src/PrinterModule.swift
+++ modules/printer-module/ios/src/PrinterModule.swift
@@ -60,7 +60,7 @@ public class PrinterModule: Module, StarDeviceDiscoveryManagerDelegate {
     }
 
     public func printData() {
-        let commands = LabelSample01_TamperProofLabel.createTamperProofLabel()
+        let commands = LabelSample02_DrinkLabel.createDrinkLabel()
 
         Task {
             do {
@@ -106,71 +106,48 @@ public class PrinterModule: Module, StarDeviceDiscoveryManagerDelegate {
     }
 }
 
-class LabelSample01_TamperProofLabel {
-    static func createTamperProofLabel() -> String {
-        guard
-            let checkedImage = UIImage(
-                named: "label_sample01_tamper_proof_label_checked.png")
-        else {
-            print(
-                "Failed to load \"label_sample01_tamper_proof_label_checked.png\"."
-            )
-            return ""
-        }
-
+class LabelSample02_DrinkLabel {
+    static func createDrinkLabel() -> String {
         let builder = StarXpandCommand.StarXpandCommandBuilder()
-
-        _ = builder.addDocument(
-            StarXpandCommand.DocumentBuilder()
-                .addPrinter(
-                    StarXpandCommand.PrinterBuilder()
-                        .styleAlignment(.center)
-                        .styleBold(true)
-                        .styleMagnification(
-                            StarXpandCommand.MagnificationParameter(
-                                width: 4, height: 4)
-                        )
-                        .actionPrintText(
-                            "SEALED\n"
-                        )
-                        .actionPrintText(
-                            "FRESH\n"
-                        )
-                        .styleBold(false)
-                        .styleMagnification(
-                            StarXpandCommand.MagnificationParameter(
-                                width: 3, height: 3)
-                        )
-                        .actionPrintText(
-                            "for Safety\n"
-                        )
-                        .actionPrintImage(
-                            StarXpandCommand.Printer.ImageParameter(
-                                image: checkedImage, width: 100)
-                        )
-                        .styleBold(true)
-                        .actionPrintText(
-                            "................\n"
-                        )
-                        .styleBold(false)
-                        .actionPrintText(
-                            "Scan to leave\n"
-                        )
-                        .actionPrintText(
-                            "a review\n"
-                        )
-                        .actionPrintQRCode(
-                            StarXpandCommand.Printer.QRCodeParameter(
-                                content: "http://starmicronics.com/"
+        
+        _ = builder.addDocument(StarXpandCommand.DocumentBuilder()
+            .addPrinter(
+                StarXpandCommand.PrinterBuilder()
+                    .styleBold(true)
+                    .actionPrintText(
+                        "Item:   1 of 3\n"
+                    )
+                    .add(
+                        StarXpandCommand.PrinterBuilder()
+                            .styleMagnification(StarXpandCommand.MagnificationParameter(width: 1, height: 2))
+                            .actionPrintText(
+                                "* Jane Smith *\n" +
+                                "Gr Icd Coffee\n"
                             )
-                            .setCellSize(8)
-                            .setLevel(.q)
-                            .setModel(.model2)
-                        )
-                        .actionCut(.partial)
-                )
+                    )
+                    .actionPrintText(
+                        "No Classic\n" +
+                        "With Whole Milk\n" +
+                        "\n" +
+                        "Time:   4:14:29 PM\n" +
+                        "Reg:    9\n" +
+                        "\n" +
+                        "--------------------------------\n"
+                    )
+                    .add(
+                        StarXpandCommand.PrinterBuilder()
+                            .styleBold(true)
+                            .actionPrintText(
+                                ">MOBILE<\n"
+                            )
+                    )
+                    .actionPrintText(
+                        "--------------------------------\n"
+                    )
+                    .actionCut(.partial)
+            )
         )
-
+        
         return builder.getCommands()
     }
 }
